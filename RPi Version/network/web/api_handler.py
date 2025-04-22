6464ec9ed74dcc8ff80467c71fe36677ccbfd1e1
Progradius <progradius@protonmail.com>
2025-04-22 18:57:43 +0200

    Fix import paths for sensor handlers and update SensorController import in API handler

--- RPi Version/network/web/api_handler.py
+++ RPi Version/network/web/api_handler.py
@@ -25,7 +25,7 @@ import re
 from urllib.parse import parse_qs, urlparse
 
 from controller import parameter_handler
-from sensorController import SensorController
+from controller.SensorController import SensorController
 from ui.pretty_console import info, warning, error
 
 
